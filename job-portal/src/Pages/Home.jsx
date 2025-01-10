d39256752c77d3fb56743c9c034e28db817a6d20
Rupala Jainish S. <127884177+KINGs2302@users.noreply.github.com>
2025-01-10 11:48:52 +0530

    Update Home.jsx

--- job-portal/src/Pages/Home.jsx
+++ job-portal/src/Pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    fetch("http://localhost:5000/all-jobs")
+    fetch("https://rapidrecruit.onrender.com/all-jobs")
       .then((res) => res.json())
       .then((data) => {
         setJobs(data);
